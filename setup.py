309352731e7b659ffedd00c1691b2e974d4143b3
dushenda_workpc <dushenda1996@163.com>
2020-03-27 01:10:40 +0800

    commit bash D:\CodePycharm\PySide\ImageRead\commit.sh

--- setup.py
+++ setup.py
@@ -13,12 +13,23 @@ import sys
 from cx_Freeze import setup, Executable
 
 path = sys.path
-packages = ["os", 'pyhdf']
+packages = ["os", 'pyhdf', 'numpy', 'pandas', 'collections', 'shiboken2', 'importlib', 'dateutil', 'distutils',
+            'encodings', 'pycallgraph', 'xmlrpc', 'http', 'email', 'urllib', 'xml', 'ctypes', 'unittest', 'logging',
+            'pytz', 'json', 'threading']
+excluded_packs = ["tkinter", 'asyncio', 'collections', 'concurrent', 'ctypes', 'curses',
+                  'dateutil', 'distutils', 'mail', 'encodings', 'html', 'http', 'importlib', 'jinja2', 'json',
+                  'lib2to3', 'logging', 'markupsafe', 'multiprocessing', 'numpy', 'pandas', 'pkg_resources', 'pycallgraph', 'pydoc_data',
+                  'pyhdf', 'pytz', 'setuptools', 'shiboken2', 'sqlite3', 'test', 'unittest', 'urllib',
+                  'win32com', 'wsgiref', 'xml', 'xmlrpc']
+for _ in packages:
+    if _ in excluded_packs:
+        excluded_packs.remove(_)
+
 include_files = ['.ui', '.images']
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
     "packages": packages,
-    "excludes": ["tkinter"],
+    "excludes": excluded_packs,
     "path": path,
     "include_files": include_files}
 
@@ -31,7 +42,6 @@ bdist_msi_options = {
     'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (company_name, product_name),
     'target_name': 'setupMDS'
 }
-
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
